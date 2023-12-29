1cb7fa4a60d74bcc696296f2eb555f0b0d4d67df
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-12-28 23:21:50 -0700

    extract wifi connection process to another function

--- src/main.cpp
+++ src/main.cpp
@@ -7,105 +7,84 @@
 #include <WiFi.h>
 #include "ams_credentials.h"
 
-
 WIEGAND wg;
 const char* apiHost = "api.ams-lti.com";
-const int httpPort = 443;
 int debug = 0;
+
+void connectToWiFi() {
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
 
 void setup() {
     Serial.begin(115200);
 
     // UNCOMMENT BELOW FOR WPA2 WIFI CONNECTION
-    // WiFi.mode(WIFI_STA);
-    // WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
 
-    // esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)username, strlen(username));
-    // esp_wifi_sta_wpa2_ent_set_username((uint8_t *)username, strlen(username));
-    // esp_wifi_sta_wpa2_ent_set_password((uint8_t *)password, strlen(password));
-    // esp_wifi_sta_wpa2_ent_enable();    
+    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)username, strlen(username));
+    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)username, strlen(username));
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)password, strlen(password));
+    esp_wifi_sta_wpa2_ent_enable();    
 
-    // WiFi.begin(ssid);
+    WiFi.begin(ssid);
 
     // UNCOMMENT BELOW FOR STANDARD WIFI CONNECTION
-    WiFi.begin(ssid, password);
+    // WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-    }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    connectToWiFi();
 
     configTime(0, 0, "pool.ntp.org", "time.nist.gov"); 
-
-    Serial.print("Synchronizing time");
-    while (time(NULL) < 946684800) { // year 2000
-        Serial.print(".");
-        delay(1000);
-    }
-    Serial.println("\nTime synchronized");
-
-
     wg.begin(34, 35);
 }
 
 void sendAttendanceRecord(const char* room, long timestamp, const char* sid) {
-    if (WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
-        http.begin("https://" + String(apiHost) + "/attendance");
-        http.addHeader("Content-Type", "application/json");
-
-        DynamicJsonDocument doc(1024);
-        doc["room"] = room;
-        doc["timestamp"] = timestamp;
-        doc["sid"] = sid;
-
-        String requestBody;
-        serializeJson(doc, requestBody);
-
-        int httpResponseCode = http.POST(requestBody);
-
-        if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println(response);
-        } else {
-            Serial.print("Error on sending POST: ");
-            Serial.println(httpResponseCode);
-        }
-
-        http.end();
-    } else {
-        Serial.println("WiFi not connected");
+    if (WiFi.status() != WL_CONNECTED) {
+        connectToWiFi();
     }
-}
 
-void dispDebug(unsigned long code) {
-    Serial.print("Wiegand HEX = ");
-    Serial.println(wg.getCode(), HEX);
+    HTTPClient http;
+    http.begin("https://" + String(apiHost) + "/attendance");
+    http.addHeader("Content-Type", "application/json");
 
-    Serial.print(", DECIMAL = ");
-    Serial.println(wg.getCode());
+    DynamicJsonDocument doc(1024);
+    doc["room"] = room;
+    doc["timestamp"] = timestamp;
+    doc["sid"] = sid;
 
-    Serial.print(", BINARY = ");
-    Serial.println(wg.getCode(), BIN);
+    String requestBody;
+    serializeJson(doc, requestBody);
 
-    Serial.print(", Type W");
-    Serial.println(wg.getWiegandType());
+    int httpResponseCode = http.POST(requestBody);
+
+    if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println(response);
+    } else {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end();
 }
 
 void loop() {
-    // if (wg.available()) {
-    //     if (debug) dispDebug(wg.getCode());
-    //     Serial.printf("%lu,%lu\n", (unsigned long)time(NULL), wg.getCode());
-    // }
-    const char* room = "COOR170"; // Replace with actual room data
-    const char* sid = "1221000031"; // Replace with actual student ID
-    long timestamp = time(NULL); // Current UNIX timestamp
+    if (wg.available()) {
+        long wiegandCode = wg.getCode();
+        String wiegandCodeStr = String(wiegandCode);
+        const char* sid = wiegandCodeStr.c_str();
+        Serial.printf("%lu,%lu\n", (unsigned long)time(NULL), wiegandCode);
+        const char* room = "COOR170"; 
+        long timestamp = time(NULL); 
 
-    sendAttendanceRecord(room, timestamp, sid);
-
-    delay(10000);
+        sendAttendanceRecord(room, timestamp, sid);
+    }
 }
